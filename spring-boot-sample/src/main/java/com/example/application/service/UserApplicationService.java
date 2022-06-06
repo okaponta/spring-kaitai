2c9f4d01dfdbed293ada3e50f5a0aef4695345f4
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-07 00:14:58 +0900

    use message.properties

--- spring-boot-sample/src/main/java/com/example/application/service/UserApplicationService.java
+++ spring-boot-sample/src/main/java/com/example/application/service/UserApplicationService.java
@@ -1,16 +1,25 @@
 package com.example.application.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedHashMap;
+import java.util.Locale;
 import java.util.Map;
 
 @Service
 public class UserApplicationService {
-    public Map<String, Integer> getGenderMap() {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    public Map<String, Integer> getGenderMap(Locale locale) {
         Map<String, Integer> genderMap = new LinkedHashMap<>();
-        genderMap.put("男性", 1);
-        genderMap.put("女性", 2);
+        String male = messageSource.getMessage("male", null, locale);
+        String female = messageSource.getMessage("female", null, locale);
+        genderMap.put(male, 1);
+        genderMap.put(female, 2);
         return genderMap;
     }
 }
--- spring-boot-sample/src/main/java/com/example/controller/SinguupController.java
+++ spring-boot-sample/src/main/java/com/example/controller/SinguupController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Locale;
 import java.util.Map;
 
 @Controller
@@ -18,8 +19,8 @@ public class SinguupController {
     private UserApplicationService userApplicationService;
 
     @GetMapping("/signup")
-    public String getSignup(Model model) {
-        Map<String, Integer> genderMap = userApplicationService.getGenderMap();
+    public String getSignup(Model model, Locale locale) {
+        Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
         model.addAttribute("genderMap", genderMap);
         return "user/signup";
     }
