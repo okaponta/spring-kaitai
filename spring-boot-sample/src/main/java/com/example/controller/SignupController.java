026f14101b86994892694d79a7f0e5bf1388936b
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-07 15:33:30 +0900

    add validation

--- spring-boot-sample/src/main/java/com/example/controller/SignupController.java
+++ spring-boot-sample/src/main/java/com/example/controller/SignupController.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,7 +32,11 @@ public class SignupController {
     }
 
     @PostMapping("/signup")
-    public String postSignup(@ModelAttribute SignupForm form) {
+    public String postSignup(Model model, Locale locale, @ModelAttribute @Validated SignupForm form,
+                             BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return getSignup(model, locale, form);
+        }
         log.info(form.toString());
         return "redirect:/login";
     }
--- spring-boot-sample/src/main/java/com/example/form/SignupForm.java
+++ spring-boot-sample/src/main/java/com/example/form/SignupForm.java
@@ -1,16 +1,27 @@
 package com.example.form;
 
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.*;
 import java.util.Date;
 
 public record SignupForm(
+        @NotBlank
+        @Email
         String userId,
+        @NotBlank
+        @Length(min = 4, max = 100)
+        @Pattern(regexp = "^[a-zA-Z0-9]+$")
         String password,
+        @NotBlank
         String userName,
         @DateTimeFormat(pattern = "yyyy/MM/dd")
+        @NotNull
         Date birthday,
+        @Min(20) @Max(100)
         Integer age,
+        @NotNull
         Integer gender
 ) {
 }
