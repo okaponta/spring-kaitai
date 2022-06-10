5554aa5363b17dd9498dff8848924785712c8f20
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-10 13:21:59 +0900

    add log to controller

--- spring-boot-sample/src/main/java/com/example/aspect/LogAspect.java
+++ spring-boot-sample/src/main/java/com/example/aspect/LogAspect.java
@@ -2,7 +2,9 @@ package com.example.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -19,5 +21,18 @@ public class LogAspect {
     @After("execution(* *..*.*UserService.*(..))")
     public void endLog(JoinPoint jp) {
         log.info("メソッド終了: " + jp.getSignature());
+    }
+
+    @Around("@within(org.springframework.stereotype.Controller)")
+    public Object startLog(ProceedingJoinPoint jp) throws Throwable {
+        log.info("メソッド開始: " + jp.getSignature());
+        try {
+            Object result = jp.proceed();
+            log.info("メソッド終了: " + jp.getSignature());
+            return result;
+        } catch (Exception e) {
+            log.error("メソッド異常終了: " + jp.getSignature());
+            throw e;
+        }
     }
 }
