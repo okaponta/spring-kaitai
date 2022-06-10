32f138eb00423f32a397c0be0419ba9b7c8aaebf
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-06-11 00:28:39 +0900

    added in memory authentication

--- spring-boot-sample/src/main/java/com/example/config/SecurityConfig.java
+++ spring-boot-sample/src/main/java/com/example/config/SecurityConfig.java
@@ -5,11 +5,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
@@ -28,7 +38,7 @@ public class SecurityConfig {
                 .failureUrl("/login?error") //失敗時
                 .usernameParameter("userId")
                 .passwordParameter("password")
-                .defaultSuccessUrl("/userList", true)
+                .defaultSuccessUrl("/user/list", true)
         ).authorizeHttpRequests(authz -> authz
                 .mvcMatchers("/login").permitAll() // 直リンクOK
                 .mvcMatchers("/user/signup").permitAll() // 直リンクOK
@@ -36,5 +46,13 @@ public class SecurityConfig {
         );
         http.csrf().disable();
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        PasswordEncoder encoder = passwordEncoder();
+        UserDetails user = User.withUsername("user").password(encoder.encode("user")).roles("GENERAL").build();
+        UserDetails admin = User.withUsername("admin").password(encoder.encode("admin")).roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
--- spring-boot-sample/src/main/java/com/example/domain/user/service/impl/UserServiceImpl.java
+++ spring-boot-sample/src/main/java/com/example/domain/user/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.domain.user.model.MUser;
 import com.example.domain.user.service.UserService;
 import com.example.repository.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,10 +16,17 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper mapper;
 
+    @Autowired
+    private PasswordEncoder encoder;
+
     @Override
     public void signup(MUser user) {
         user.setDepartmentId(1);
         user.setRole("ROLE_GENERAL");
+        // パスワード暗号化
+        String rawPassword = user.getPassword();
+        user.setPassword(encoder.encode(rawPassword));
+
         mapper.insertOne(user);
     }
 
@@ -35,7 +43,8 @@ public class UserServiceImpl implements UserService {
     @Transactional
     @Override
     public void updateUserOne(String userId, String password, String userName) {
-        mapper.updateOne(userId, password, userName);
+        String encryptPassword = encoder.encode(password);
+        mapper.updateOne(userId, encryptPassword, userName);
         // 例外を発生させる
         // int i = 1 / 0;
     }
